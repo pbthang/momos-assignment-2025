5955e8a8e31c4653c8a81af2989c6dffd11232c5
Ba Thang Pham <BaThang.Pham@mrisoftware.com>
2025-12-22 17:58:49 +0800

    Add React Query

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import { useQuery } from "@tanstack/react-query";
+
 export function App() {
-    return <div>Hello world</div>;
+    const { data, error, isLoading } = useQuery({
+        queryKey: ["exampleData"],
+        queryFn: async () => {
+            // Simulate fetching data
+            return new Promise<string>((resolve) =>
+                setTimeout(() => resolve("Fetched Data"), 1000)
+            );
+        },
+    });
+
+    return (
+        <main className="bg-background container px-4 py-4">
+            <h1 className="text-3xl font-bold underline">Hello world!</h1>
+            <div>
+                Welcome to your new Remix + Tailwind CSS + TypeScript app.
+            </div>
+        </main>
+    );
 }
 
 export default App;
